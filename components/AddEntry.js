3ccbb0e488636bf9e24a9f821d323a89444678ad
mohmdalfaha <mohmd1919@gmail.com>
2019-03-14 03:48:22 +0300

    Displaying Icons with their corresponding functionality

---
+++ components/AddEntry.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import { View } from 'react-native'
+import { getMetricMetaInfo } from '../utils/helpers'
+import Slider from './Slider'
+import Steppers from './Steppers'
+
+export default class AddEntry extends React.Component {
+  state = {
+    run:0,
+    bike:0,
+    swim:0,
+    sleep:0,
+    eat:0,
+  }
+
+increment = (metric) => {
+  const { max, step } = getMetricMetaInfo(metric)
+
+  this.setState((state) => {
+    const count = state[metric] + step
+
+    return {
+      ...state,
+      [metric]: count > max ? max : count
+    }
+  })
+}
+
+
+decrement = (metric) => {
+
+  this.setState((state) => {
+    const count = state[metric] - getMetricMetaInfo(metric).step
+
+    return {
+      ...state,
+      [metric]: count < 0 ? 0 : c0ount
+    }
+  })
+}
+
+slide = (metric, value) => {
+  this.setState(() => ({
+    [metric]: value
+  }))
+}
+
+render(){
+  const metaInfo = getMetricMetaInfo()
+
+  return (
+    <View>
+      {Object.keys(metaInfo).map((key) => {
+        const { getIcon, type, ...rest } = metaInfo[key]
+        const value = this.state[key]
+
+        return (
+          <View key={key}>
+            {getIcon()}
+            {type === 'slider'
+            ? <Slider
+                value={value}
+                onChange={(value) => this.state(key, value)}
+                {...rest}
+              />
+            : <Steppers
+                value={value}
+                onIncrement={() => this.increment(key)}
+                onDecrement={() => this.decrement(key)}
+                {...rest}
+              />
+            }
+          </View>
+          )
+      })}
+    </View>
+    )
+}
+}
