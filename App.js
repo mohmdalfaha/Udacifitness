6944814f143ba9ef6c100d44e0117ddde8e45650
mohmdalfaha <mohmd1919@gmail.com>
2019-03-24 01:08:08 +0300

    feat: Add tabs to navigate between AddEntry and History components

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
 import React from 'react'
-import { View,Text } from 'react-native'
+import { View,Text, Platform } from 'react-native'
 import AddEntry from './components/AddEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import History from './components/History'
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import { purple, white } from './utils/colors'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
 
-export default class App extends React.Component {
+const RouteConfigs = {
+  History: {
+    screen: History,
+    navigationOptions: {
+      tabBarLabel: 'History',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+    },
+  },
+  AddEntry: {
+    screen: AddEntry,
+    navigationOptions: {
+        tabBarLabel: 'Add Entry',
+        tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+      }
+    }
+};
+
+const TabNavigatorConfig = {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: purple,
+    style: {
+      height: 56,
+      backgroundColor: white,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3,
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1,
+    },
+  },
+};
+
+const Tabs = createBottomTabNavigator(RouteConfigs, TabNavigatorConfig)
+
+
+const TabsContainer = createAppContainer(Tabs)
+
+ export default class App extends React.Component {
   render() {
     return (
      <Provider store={createStore(reducer)}>
       <View style={{flex: 1}}>
         <View style={{height:20}}/>
-       <History/>
+       <TabsContainer/>
       </View>
      </Provider>
     )
   }
 }
+
